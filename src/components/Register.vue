acfa2c66782238e5b1c060b6256a18cd3d6c6419
farhanhrywn <farhanharyawan@gmail.com>
2020-11-12 00:12:18 +0700

    Login page

---
+++ src/components/Register.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Ini form register</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class='login'>
+      <LoginForm/>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/Login.vue'
+// import Register from '../components/Register'
+export default {
+  name: 'Login',
+  components: {
+    LoginForm
+  }
+
+}
+</script>
+
+<style>
+
+</style>
